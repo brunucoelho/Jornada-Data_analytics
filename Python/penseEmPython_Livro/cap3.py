c96ba687a711de2584b8040de50fa214ef40f533
Bruno Coelho <112502980+brunucoelho@users.noreply.github.com>
2023-04-11 12:22:01 -0300

    Questão 1 - cap3

--- Python/penseEmPython_Livro/cap3.py
+++ Python/penseEmPython_Livro/cap3.py
@@ -1,6 +1,8 @@
 #Exercício 3.1
 #Escreva uma função chamada right_justify, que receba uma string chamada s como parâmetro e exiba a string com espaços suficientes à frente para que a última letra da string esteja na coluna 70 da tela: >>> right_justify('monty')                                                                 monty
-
+def right_justify(str):
+    texto = str
+    print(' '*(70-len(texto))+texto)
 #Exercício 3.2
 #Um objeto de função é um valor que pode ser atribuído a uma variável ou passado como argumento. Por exemplo, do_twice é uma função que toma um objeto de função como argumento e o chama duas vezes:
 #def do_twice(f):
@@ -16,5 +18,3 @@
 
 #Dica: para exibir mais de um valor em uma linha, podemos usar uma sequência de valores separados por vírgula: print('+', '-')
 #Por padrão, print avança para a linha seguinte, mas podemos ignorar esse comportamento e inserir um espaço no fim, desta forma: python print('+', end='') print('-') A saída dessas instruções é + -. Uma instrução print sem argumento termina a linha atual e vai para a próxima linha.
-
-
