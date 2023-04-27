ff93ae9fd49b911a4675c22a06616cd20fcda359
Bruno Coelho <112502980+brunucoelho@users.noreply.github.com>
2023-04-27 12:17:35 -0300

    questão 2. cap3

--- Python/penseEmPython_Livro/cap3.py
+++ Python/penseEmPython_Livro/cap3.py
@@ -1,8 +1,10 @@
 #Exercício 3.1
 #Escreva uma função chamada right_justify, que receba uma string chamada s como parâmetro e exiba a string com espaços suficientes à frente para que a última letra da string esteja na coluna 70 da tela: >>> right_justify('monty')                                                                 monty
+
 def right_justify(str):
     texto = str
     print(' '*(70-len(texto))+texto)
+
 #Exercício 3.2
 #Um objeto de função é um valor que pode ser atribuído a uma variável ou passado como argumento. Por exemplo, do_twice é uma função que toma um objeto de função como argumento e o chama duas vezes:
 #def do_twice(f):
@@ -12,6 +14,23 @@ def right_justify(str):
 #def print_spam():
 #print('spam')
 #do_twice(print_spam)
+
+def do_twice(f, valor):
+    f(valor)
+    f(valor)
+
+def print_twice(valor):
+    print(valor)
+    print(valor)
+
+def do_four(f, valor):
+    do_twice(f, valor)
+    do_twice(f, valor)
+
+do_twice(print, 'spam')
+print('-'*7)
+
+do_four(print, 'spam')
 
 #Exercício 3.3
 #Escreva uma função que desenhe uma grade como a seguinte:
