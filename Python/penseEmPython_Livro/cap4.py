b29a83e58cbdfd3dcc7fc81fb50c7c63ed593a6f
Bruno Coelho <112502980+brunucoelho@users.noreply.github.com>
2023-04-28 12:20:57 -0300

    Atualização. Cap 4

--- Python/penseEmPython_Livro/cap4.py
+++ Python/penseEmPython_Livro/cap4.py
@@ -19,22 +19,32 @@ def square(t, length):
     import turtle
 
     t = turtle.Turtle()
-    t.shape('turtle')
+    t.shape()
     for i in range(4):
-        t.forward(length)
-        t.right(90)
-    t.left(90)
+        t.fd(length)
+        t.rt(90)
+    t.lt(90)
 
 #3. Faça uma cópia do square e mude o nome para polygon. Acrescente outro parâmetro chamado n e altere o corpo para que desenhe um polígono regular de n lados. Dica: os ângulos exteriores de um polígono regular de n lados são 360/n graus.
-def polygon(n, length):
+
+def polygon(t, length, n):
     import turtle
 
     t = turtle.Turtle()
     t.shape('turtle')
-    for i in range(4):
-        t.forward(length)
-        t.right(90)
-    t.left(90)
+    for i in range(n):
+        t.fd(length)
+        t.rt(360/n)
+    t.lt(length)
+
+
 #4. Escreva uma função chamada circle que use o turtle, t e um raio r como parâmetros e desenhe um círculo aproximado ao chamar polygon com um comprimento e número de lados adequados. Teste a sua função com uma série de valores de r.Dica: descubra a circunferência do círculo e certifique-se de que length * n = circumference.
 
+def circle(t,r):
+    if r > 10:
+        r = 10
+        polygon(t, r*10, 15) #Aqui se chama a função polygon, caso for executar somente esse exercicio, deve colocar as duas funções (polygon e circle) no scrip.
+    else:
+        polygon(t, r*10, 15)
+    
 #5. Faça uma versão mais geral do circle chamada arc, que receba um parâmetro adicional de angle, para determinar qual fração do círculo deve ser desenhada. angle está em unidades de graus, então quando angle=360, o arc deve desenhar um círculo completo.
