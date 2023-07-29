3df353cac65eb819215e1891b68a5ca0873cabdb
brunucoelho <bruno-coelho@hotmail.com>
2023-07-29 12:40:11 -0300

    organização

--- Python/Hashtag treinamentos/Python + SQL/CRUD - Create.py
+++
@@ -1,9 +0,0 @@
-import pyodbc
-print(pyodbc.drivers())
-
-dados_conexao = ("Driver={SQLite3 ODBC Driver};""Server=localhost;""Database=salarios.sqlite")
-#caso precise de login e senha:
-#       ..."UID=Login;""PWD=Senha;")
-
-conexao = pyodbc.connect(dados_conexao)
-print("Conexão bem sucedida")
--- Python/Hashtag treinamentos/fuction.py
+++
@@ -1,3 +0,0 @@
-def calculo_de_stockout():
-    nov_lista = []
-    if 
