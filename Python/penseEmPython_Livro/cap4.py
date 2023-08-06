11e1661f0d0ab4c30235bb3d693e5b9c6cc0b31d
brunucoelho <bruno-coelho@hotmail.com>
2023-08-06 11:40:48 -0300

    atualização, cap4.

--- Python/penseEmPython_Livro/cap4.py
+++ Python/penseEmPython_Livro/cap4.py
@@ -45,3 +45,31 @@ def circle(t,r):
         polygon(t, r*10, 15)
     
 #5. Faça uma versão mais geral do circle chamada arc, que receba um parâmetro adicional de angle, para determinar qual fração do círculo deve ser desenhada. angle está em unidades de graus, então quando angle=360, o arc deve desenhar um círculo completo.
+def arc(t,r,angle):
+    import turtle
+
+    t = turtle.Turtle()
+    for i in range():
+
+"""Exercício 4.1
+Baixe o código deste capítulo no site http://thinkpython2.com/code/polygon.py.
+1. Desenhe um diagrama da pilha que mostre o estado do programa enquanto executa circle (bob, radius). Você pode fazer a aritmética à mão ou acrescentar instruções print ao código.
+
+2. A versão de arc na seção 4.7 - Refatoração não é muito precisa porque a aproximação linear do círculo está sempre do lado de fora do círculo verdadeiro. Consequentemente ,Turtle acaba ficando alguns píxeis de distância do destino correto. Minha solução mostra um modo de reduzir o efeito deste erro. Leia o código e veja se faz sentido para você. Se desenhar um diagrama, poderá ver como funciona.
+"""
+
+"""Exercício 4.2
+Escreva um conjunto de funções adequadamente geral que possa desenhar flores como as da Figura 4.1."""
+
+
+"""Exercício 4.3
+Escreva um conjunto de funções adequadamente geral que possa desenhar formas como as da Figura 4.2."""
+
+'''Exercício 4.4
+as letras do alfabeto podem ser construídas a partir de um número moderado de elementos básicos, como linhas verticais e horizontais e algumas curvas. Crie um alfabeto que possa ser desenhado com um número mínimo de elementos básicos e então escreva funções que desenhem as letras. Você deve escrever uma função para cada letra, com os nomes draw_a, draw_b etc., e colocar suas funções em um arquivo chamado letters.py. Você pode baixar uma “máquina de escrever de turtle” no site http://thinkpython2.com/code/typewriter.py para ajudar a testar o seu código. Você pode ver uma solução no site http://thinkpython2.com/code/letters.py; ela também exige http://thinkpython2.com/code/polygon.py.
+'''
+
+
+"""Exercício 4.5
+Leia sobre espirais em https://pt.wikipedia.org/wiki/Espiral; então escreva um programa que desenhe uma espiral de Arquimedes (ou um dos outros tipos). 
+[1] _turtle graphics_ ou gráficos de tartaruga é o sistema de desenho popularizado pela linguagem Logo, onde os comandos movimentam um cursor triangular pela tela, conhecido como turtle ou tartaruga. A tartaruga deixa um rastro à medida que é movimentada, e é com esses rastros que se forma um desenho. Diferente dos sistemas usuais de desenho em computação gráfica, o sistema turtle graphics não exige o uso de coordenadas cartesianas."""
