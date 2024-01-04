4986304a27d57056d8189a8578ebc91a92e9bee0
Bruno Coelho <112502980+brunucoelho@users.noreply.github.com>
2024-01-03 21:49:01 -0300

    Update Cap6 ao 8.py

--- Python/Livro_introAProgramacaoComPython/Cap6 ao 8.py
+++ Python/Livro_introAProgramacaoComPython/Cap6 ao 8.py
@@ -1,13 +1,65 @@
 """Exercício 6.1 Modifique o programa da listagem 6.6 para ler 7 notas em vez de 5."""
 
+notas = [0,0,0,0,0,0,0]
+soma = 0
+x = 0
+while x < 5:
+  notas[x] = float(input('Nota %d: ' %x))
+  soma += notas[x]
+  x += 1
+x = 0
+while x < 5:
+  print('Nota %d: %6.2f' % (x,notas[x]))
+  x += 1
+print('Média: %5.2f' % (soma/x))
+
 """Exercício 6.2 Faça um programa que leia duas listas e que gere uma terceira com os elementos das duas primeiras."""
+
+lista1 = [1, 2, 3, 4]
+lista2 = [5, 6, 7, 4, 8]
+lista3 = l1+l2
+print(lista3)
 
 """ Exercício 6.3 Faça um programa que percorra duas listas e gere uma terceira sem elementos repetidos."""
 
+lista_new = []
+lista3 = lista1+lista2
+for l in lista3:
+  lista_new.append(l)
+list(set(lista_new))
+
 """Exercício 6.4 O que acontece quando não verificamos se a lista está vazia antes de chamarmos o método pop?"""
+
+"""Se não verificarmos que a lista está vazia antes de chamarmos o pop(),
+o programa para com uma mensagem de erro, informando que tentamos retirar um elemento de uma lista vazia."""
+# IndexError: pop from empty list
 
 """Exercício 6.5 Altere o programa da listagem 6.21 de forma a poder trabalhar com vários comandos digitados de uma só vez. Atualmente, apenas um comando pode ser inserido por vez. Altere-o de forma a considerar operação como uma string.
 Exemplo: FFFAAAS significaria três chegadas de novos clientes, três atendimentos e, finalmente, a saída do programa."""
+
+último = 10
+ultimo is True
+fila = list(range(1,último+1))
+while True:
+  print("\nExistem %d clientes na fila" % len(fila))
+  print("Fila atual:", fila)
+  print("Digite F para adicionar um cliente ao fim da fila,")
+  print("ou A para realizar o atendimento. S para sair.")
+  operacao = input("Operacao (F, A ou S):")
+  for i, teste in enumerate(operacao):
+    if operacao[i] == "A":
+      if(len(fila))>0:
+        atendido = fila.pop(0)
+        print("Cliente %d atendido" % atendido)
+      else:
+        print("Fila vazia! Ninguém para atender.")
+    elif operacao[i] == "F":
+      último+=1 # Incrementa o ticket do novo cliente
+      fila.append(último)
+    elif operacao[i] == "S":
+      break
+    else:
+      print("Operacao inválida! Digite apenas F, A ou S!")
 
 """Exercício 6.6 Modifique o programa para trabalhar com duas filas. Para facilitar seu trabalho, considere o comando A para atendimento da fila 1; e B, para atendimento da fila 2. O mesmo para a chegada de clientes: F para fila 1; e G, para fila 2"""
 
